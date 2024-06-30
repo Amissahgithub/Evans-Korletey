92adbab4604949cb88efdacf1081ce6956d304a2
Amissahgithub <138082043+Amissahgithub@users.noreply.github.com>
2024-06-30 14:24:16 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,372 @@
+
+
+<?php include("header.php");?>
+
+
+<div class="slider-section">
+
+
+<!-- revolution slider -->
+<section class="no-top no-bottom" aria-label="section-slider">
+<!-- home -->
+<div class="fullwidthbanner-container">
+<div id="revolution-slider-half">
+              <ul>
+
+                  <li data-transition="random" data-slotamount="10" data-masterspeed="1200" data-delay="5000">
+                      <!--  BACKGROUND IMAGE -->
+                      <img src="img/2 (3).jpg" alt="" data-start="0" data-bgposition="center center" data-kenburns="on" data-duration="10000" data-ease="Linear.easeNone" data-bgfit="100" data-bgfitend="100" data-bgpositionend="center center"/>
+                      
+					  
+					   <div class="tp-caption slide_mask  sfl tp-resizeme "
+			data-x="0"
+			data-y="0" 
+			data-speed="500"
+			data-start="10"
+			data-easing="Power3.easeInOut"
+			data-splitin="none"
+			data-splitout="none"
+			data-elementdelay="0.1"
+			data-endelementdelay="0.1"
+			data-endspeed="300"
+			style=" max-width: auto; max-height: auto; white-space: nowrap;">
+		</div>
+					  
+					  <div class="tp-caption slide-big-heading sft"
+                          data-x="30"
+                          data-y="170"
+                          data-speed="800"
+                          data-start="400"
+                          data-easing="easeInOutExpo"
+                          data-endspeed="450">
+                        An Expert Electrical<BR> Service you can Trust
+                      </div>
+					  
+					    <div class="tp-caption slide-sub-heading sft"
+                          data-x="30"
+                          data-y="320"
+                          data-speed="1000"
+                data-start="800"
+                data-easing="easeOutExpo"
+                          data-endspeed="400">
+                          Creating architectural and creative solutions for you
+                      </div>
+ 
+                  </li>
+                  
+                  <li data-transition="fade" data-slotamount="10" data-masterspeed="1200" data-delay="5000">
+                      <!--  BACKGROUND IMAGE -->
+                      <img src="img/2 (3).jpg" alt="" data-start="0" data-bgposition="center center" data-kenburns="on" data-duration="10000" data-ease="Linear.easeNone" data-bgfit="100" data-bgfitend="100" data-bgpositionend="center center"/>
+                        
+						 <div class="tp-caption slide_mask  sfl tp-resizeme "
+			data-x="0"
+			data-y="0" 
+			data-speed="500"
+			data-start="10"
+			data-easing="Power3.easeInOut"
+			data-splitin="none"
+			data-splitout="none"
+			data-elementdelay="0.1"
+			data-endelementdelay="0.1"
+			data-endspeed="300"
+			style=" max-width: auto; max-height: auto; white-space: nowrap;">
+		</div>
+						
+						<div class="tp-caption slide-big-heading sft"
+                          data-x="30"
+                          data-y="170"
+                          data-speed="800"
+                          data-start="400"
+                          data-easing="easeInOutExpo"
+                          data-endspeed="450">
+                        An Expert Electrical<BR> Service you can Trust
+                      </div>
+					  
+					    <div class="tp-caption slide-sub-heading sft"
+                          data-x="30"
+                          data-y="320"
+                          data-speed="1000"
+                data-start="800"
+                data-easing="easeOutExpo"
+                          data-endspeed="400">
+                          Creating architectural and creative solutions for you
+                      </div>
+
+                      <div class="tp-caption btn-slider sfb"
+                          data-x="30"
+                          data-y="390"
+                          data-speed="400"
+                          data-start="800"
+                          data-easing="easeInOutExpo">
+                          <span class="shine"></span><a href="#" >More Detail</a>
+                      </div>
+
+                  </li>
+
+                 
+                 
+              </ul>
+              <div class="tp-bannertimer hide"></div>
+</div>
+</div>
+<!-- home end -->
+</section>
+<!-- revolution slider end -->
+    <div class="scroll"> <a href="#content2" class="go-down"></a></div>
+</div>
+
+
+
+	<section class="about-style-two py-5" id="content2">
+<div class="container">
+<div class="row align-items-center">
+<div class="col-lg-4">
+<div class="about-img">
+<img src="img/2 (1).jpg"  class="img-fluid" alt="about image">
+</div>
+</div>
+<div class="col-lg-8">
+<div class="about-text">
+<div class="section-title">
+<span>Our Company</span>
+<h2>Barackjnr Electricals Building Yours Dream Since 2021</h2>
+</div>
+<p>Barackjnr Electrical is an engineering company which has been set to bring electrical solution at your
+   doorstep. We have well trained engineers whose expertise is in power. We have worked in various
+    government and private sectors with more than 3 decades of experience.</p>
+
+<div class="d-flex">
+             
+
+                    <div class="logo_school mr-5">
+                        <img src="img/mission.svg" width="40">
+                        <h5>Mission</h5>
+						<p> " 
+  
+  
+  Our mission to empower young engineers to be innovative and contribute to the power
+   stability and aslo encourage the use of state of the art devices for project at large.</p>
+                    </div>
+
+                    <div class="logo_school">
+                        <img src="img/vision.svg" width="40">
+                        <h5 class="text-left">
+                            Vision
+                           
+                        </h5>
+						<p> Our vision is to make a significant impact in the power and security sector and 
+  provide state power and comfort for our clients.”</p>
+                    </div>
+                </div>
+</div>
+</div>
+</div>
+</div>
+</section>
+	
+	
+	
+	
+	<section class="service-style-three  pb-70">
+<div class="container">
+<div class="section-title text-center">
+<span>Our Services</span>
+<h2>Barackjnr Electricals Provides All Building Services</h2>
+<p>We are also embedded
+  with engineers that can can build any device you present to be developed for you. Some of the 
+   services we offer are displayed below. </p>
+</div>
+<div class="row">
+<div class="col-lg-4 col-sm-6">
+<div class="service-card text-center">
+<div class="service-img urban">
+</div>
+<div class="service-content">
+  <img src="img/s1.svg"/>
+<h3>House Wiring</h3>
+<p>We do complete wiring of houses, factories, warehouse and so much more.
+  Our house wiring entends to installing the finishing works and appliances</p>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-sm-6">
+<div class="service-card text-center">
+<div class="service-img road">
+</div>
+<div class="service-content">
+  <img src="img/s4.svg"/>
+  <h3>Installation of solar pannels and servicing</h3>
+<p>We install  solar pannels and servicing for all purposes </p>
+
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-sm-6">
+<div class="service-card text-center">
+<div class="service-img industrial">
+</div>
+<div class="service-content">
+  <img src="img/s6.svg"/>
+  <h3>Build And Repair Of Automatic Changeover Switches</h3>
+<p></p>we build and repair automatic
+change over switches for any size and power rating modified with state of the art automation to monitor fuel levels.
+
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-sm-6">
+<div class="service-card text-center">
+<div class="service-img construction">
+</div>
+<div class="service-content">
+  <img src="img/s1.svg"/>
+  <h3>CCTV And Other Security Installation</h3>
+<p>we install
+  state of the art security cameras smart doors and electric fence to beef up
+   security anywhere you want it to be installed.</p>
+
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-sm-6">
+<div class="service-card text-center">
+<div class="service-img structure">
+</div>
+<div class="service-content">
+  <img src="img/s2.svg"/>
+  <h3>Monitoring Of Power</h3>
+<p>We do power monitoring at all kinds of level</p>
+
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-sm-6">
+<div class="service-card text-center">
+<div class="service-img water">
+</div>
+<div class="service-content">
+  <img src="img/s6.svg"/>
+  <h3>Generator Servicing And Installation</h3>
+<p> we sell,
+  buy, install service and repair generators of all kinds and sizes.</p>
+
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+	
+	
+	
+	
+	<section class="facilities-section">
+<div class="container-fluid">
+<div class="row">
+<div class="col-lg-5 offset-lg-1 p-lg-0">
+<div class="facililties-text pt-100 pb-70">
+<div class="section-title">
+<span>More Facilities</span>
+<h2>Barackjnr Electrical Services You More</h2>
+<p></p>
+</div>
+<ul>
+<li>Best Quality Work</li>
+<li>Great Understanding</li>
+<li>Emergency Support</li>
+<li>Top Quality Equipment</li>
+<li>Experienced Workers</li>
+<li>Reasonable</li>
+<li>Budget-Friendly</li>
+<li>Responsible </li>
+</ul>
+<div class="theme-btn">
+<a href="contactus.html" class="default-btn">
+Contact Us
+</a>
+</div>
+</div>
+</div>
+<div class="col-lg-6 p-lg-0">
+<div class="facilities-img">
+</div>
+</div>
+</div>
+</div>
+</section>
+	
+	
+<section class="project-section pt-100 pb-100">
+<div class="container">
+<div class="section-title text-center">
+<span>Our Projects</span>
+<h2>We Have Some Completed Projects</h2>
+
+</div>
+ <div class="slick-slider project-slider">
+      <div>
+         <div class="gal-item"> 
+                <!-- GALLERY IMAGE --> 
+                <img src="Automatic-ChangeOver_Switch-320x320.jpg" alt="" class="img-fluid"> 
+                
+              </div>  
+      </div>
+<div>
+       <div class="gal-item"> 
+                <!-- GALLERY IMAGE --> 
+                <img src="CCTV.jpeg" alt="" class="img-fluid"> 
+                
+              </div>  
+      </div>
+<div>
+      <div class="gal-item"> 
+                <!-- GALLERY IMAGE --> 
+                <img src="solar panel.jpeg" alt="" class="img-fluid"> 
+              
+                
+              </div>
+      </div>
+<div>
+      <div class="gal-item"> 
+                <!-- GALLERY IMAGE --> 
+                <img src="switchbox-2652780_1280.jpg" alt="" class="img-fluid"> 
+                
+               
+              </div>
+      </div>
+ </div>
+ <div class="gal-item"> 
+  <!-- GALLERY IMAGE --> 
+  <img src="GEN.jpeg" alt="" class="img-fluid"> 
+  
+ </div>
+
+</div>
+</div>
+
+
+</div>
+</div>
+</section>
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+	
+	
+	
+
+
+
+<?php include("footer.php");?>
+	
